240c5f5d47f0827064a8b6b97546f6cac6d14f35
Patricia Selpa <patricia.selpa@gmail.com>
2020-09-29 21:12:03 -0300

    Exercícios 1, 2, 4 e 5.

--- Prova.cs
+++ Prova.cs
@@ -1,6 +1,7 @@
 using System;
+using System.Collections.Generic;
 
-namespace second_arrays_list_tests
+namespace Primeira_prova
 {
     class Prova
     {
@@ -11,7 +12,55 @@ namespace second_arrays_list_tests
 
             double dailyWage = wagePerHour * (hoursWork + (minutesWork/60));
             return dailyWage;
+        }
 
+
+        public string Exercise2(string[] word)
+        {
+            // Dado que a aplicação está preparada. Quando o usuário chamar o exercício 2, então a 
+            //aplicação deverá receber uma palavra uma letra por vez. Retornar a palavra inteira
+
+            string resultWord = "";
+
+            foreach (var item in word)
+            {
+                resultWord += item;
+            }
+            return resultWord;
+        }
+
+
+        // ************** TESTAR O 3 DEPOIS **************
+
+        public bool Exercise4(int year)
+        {
+            // Dado que a aplicação está preparada. Quando o usuário chamar o exercício 4, então a aplicação
+            //deverá receber o ano de nascimento do usuário e retornar se ele é ou não maior de idade.
+            bool answer = false;
+
+            if (2020 - year >= 18)
+            {
+                answer = true;
+            }
+            return answer;
+        }
+
+
+        public List<object> Exercise5(object[] array)
+        {
+            // Dado que a aplicação está preparada. Quando o usuário chamar o exercício 4, então a aplicação deverá receber 5 números e
+            //armazenar em um array. Se houver um valor inválido, armazenar “null” no índice. Ao final, imprimir apenas os números válidos.
+
+            var resultList = new List<object>{};
+
+            foreach (var item in array)
+            {
+               if (item != null)
+               {
+                    resultList.Add(item);
+               }
+            }
+            return resultList;
         }
     }
 }
--- ProvaTests.cs
+++ ProvaTests.cs
@@ -1,13 +1,79 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
 
-namespace Primeira_prova_tests
+namespace Primeira_prova
 {
     public class ProvaTests
     {
-        [Fact]
-        public void should_return_the_wage_of_a_worker_based_on_how_many_hours_they_work()
+        [Theory]
+        [InlineData(8, 0, 5, 40)]
+        [InlineData(9, 30, 10, 95)]
+        [InlineData(4, 0, 6, 24)]
+        public void should_return_the_daily_wage_of_a_worker_based_on_how_many_hours_they_work(double hoursWork, double minutesWork, double wagePerHour, double expected)
         {
-            
+            // Dado / Setup
+            var exercises = new Prova();
+
+            // Quando / Ação
+            double returnedValue = exercises.Exercise1(hoursWork, minutesWork, wagePerHour);
+
+            // Deve / Asserções
+            Assert.Equal(expected, returnedValue);
         }
+
+
+        [Theory]
+        [InlineData(new string[5]{"S","w","o","r","d"}, "Sword")]
+        [InlineData(new string[5]{"A","r","m","o","r"}, "Armor")]
+        [InlineData(new string[5]{"S","t","a","f","f"}, "Staff")]
+        public void should_return_a_word_from_a_list_of_strings(string[] word, string expected)
+        {
+            // Dado / Setup
+            var exercises = new Prova();
+
+            // Quando / Ação
+            var returnedValue = exercises.Exercise2(word);
+
+            // Deve / Asserções
+            Assert.Equal(expected, returnedValue);
+        }
+
+        // ************** TESTAR O 3 DEPOIS **************
+
+
+        [Theory]
+        [InlineData(2016, false)]
+        [InlineData(1992, true)]
+        [InlineData(1858, true)]
+        [InlineData(2074, false)]
+        public void should_return_if_a_person_is_over_aged(int year, bool expected)
+        {
+            // Dado / Setup
+            var exercises = new Prova();
+
+            // Quando / Ação
+            bool returnedValue = exercises.Exercise4(year);
+
+            // Deve / Asserções
+            Assert.Equal(expected, returnedValue);
+        }
+
+
+        [Theory]
+        [InlineData(new object[5]{3, null, 5, 10, null}, new object[3]{3, 5, 10})]
+        public void should_return_the_valid_numbers_of_a_list(object[] array, object[] expected)
+        {
+            // Dado / Setup
+            var exercises = new Prova();
+
+            // Quando / Ação
+            var returnedValue = exercises.Exercise5(array);
+
+            // Deve / Asserções
+            Assert.Equal(expected, returnedValue);
+        }
+
+        
     }
 }
