acd19351b1bc9038891c2bde021da5e1515db1fd
Patricia Selpa <patricia.selpa@gmail.com>
2020-10-01 19:35:37 -0300

    Exercícios 6, 7 e 8.

--- Prova.cs
+++ Prova.cs
@@ -48,7 +48,7 @@ namespace Primeira_prova
 
         public List<object> Exercise5(object[] array)
         {
-            // Dado que a aplicação está preparada. Quando o usuário chamar o exercício 4, então a aplicação deverá receber 5 números e
+            // Dado que a aplicação está preparada. Quando o usuário chamar o exercício 5, então a aplicação deverá receber 5 números e
             //armazenar em um array. Se houver um valor inválido, armazenar “null” no índice. Ao final, imprimir apenas os números válidos.
 
             var resultList = new List<object>{};
@@ -62,5 +62,74 @@ namespace Primeira_prova
             }
             return resultList;
         }
+
+
+        public double Exercise6(double[] wageList)
+        {
+            // Dado que a aplicação está preparada. Quando o usuário chamar o exercício 6, então a aplicação deverá 
+            //retornar a média salarial dos funcionários de uma empresa.
+
+            double averageWage = 0;
+            double totalWage = 0;
+            int counter = 0;
+
+            foreach (var item in wageList)
+            {
+                if (item > 0)
+                {
+                    totalWage += item;
+                    counter++;
+                }
+            }
+            averageWage = totalWage/counter;
+            return averageWage;
+        }
+
+
+        public double Exercise7(int[] userState)        
+        {
+            //Dado que a aplicação está preparada. Quando o usuário chamar o exercício 7, então a aplicação deverá receber o código da UF
+            //do estado onde nasceram 5 usuários. Retornar a porcentagem de usuários que nasceram em Santa Catarina.
+            //(42, SC), \n(11, RO), \n(12, AC), \n(13, AM), \n(14, RR), \n(15, PA), \n(16, AP), \n(17, TO), \n(21, MA), \n(22, PI), \n(23, CE), \n(24, RN), \n(25, PB), \n(26, PE), \n(27, AL), \n(28, SE), \n(29, BA), \n(31, MG), \n(32, ES), \n(33, RJ), \n(35, SP), \n(41, PR), \n(43, RS), \n(50, MS), \n(51, MT), \n(52, GO), \n(53, DF)
+
+            double percent = 0;
+            double counter = 0;
+
+            foreach (var item in userState)
+            {
+                if (item == 42)
+                {
+                    counter++;
+                }
+            }
+            percent = (counter/5) * 100;
+            return percent;
+        }
+
+
+        public string[] Exercise8(string[] morningClass, string[] eveningClass)
+        {
+            //Dado que a aplicação está preparada. Quando o usuário chamar o exercício 8, então a aplicação deverá receber as listas de
+            //alunos das turmas Matutino e Vespertino. Cada turma possui 5 alunos. Ao final retorne apenas os nomes dos alunos que estão
+            //estudando em período integral.
+
+            var allDayClass = new string[5];
+            int counter = 0;
+        
+            foreach (var item in morningClass)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (item == eveningClass[i])
+                    {
+                        allDayClass[counter] = item;
+                        counter++;
+                    }
+                }
+            }
+            return allDayClass;
+        }
+
+        
     }
 }
--- ProvaTests.cs
+++ ProvaTests.cs
@@ -74,6 +74,55 @@ namespace Primeira_prova
             Assert.Equal(expected, returnedValue);
         }
 
-        
+
+        [Theory]
+        [InlineData(new double[3]{500, 1000, 1500}, 1000)]
+        [InlineData(new double[4]{800, 750, 1100, 700}, 837.50)]
+        [InlineData(new double[3]{-25, 2000, 1500}, 1750)]
+        public void should_return_the_average_wage_of_a_group_of_workers(double[] wageList, double expected)
+        {
+            // Dado / Setup
+            var exercises = new Prova();
+
+            // Quando / Ação
+            var returnedValue = exercises.Exercise6(wageList);
+
+            // Deve / Asserções
+            Assert.Equal(expected, returnedValue);
+        }
+
+
+        [Theory]
+        [InlineData(new int[5]{42, 11, 17, 22, 42}, 40)]
+        [InlineData(new int[5]{17, 25, 33, 61, 12}, 0)]
+        [InlineData(new int[5]{14, 25, 42, 18, 45}, 20)]
+        [InlineData(new int[5]{17, 42, 42, 42, 31}, 60)]
+        public void should_return_the_percentage_of_people_from_SC(int[] userState, double expected)
+        {
+            // Dado / Setup
+            var exercises = new Prova();
+
+            // Quando / Ação
+            var returnedValue = exercises.Exercise7(userState);
+
+            // Deve / Asserções
+            Assert.Equal(expected, returnedValue);
+        }
+
+
+        [Theory]
+        [InlineData(new string[5]{"Ana", "Josi", "Cláudia", "Fernanda", "Mabel"}, new string[5]{"Mabel", "Josi", "Rafa", "Cláudia", "Clara"}, new string[5]{"Josi", "Cláudia", "Mabel", null, null})]
+        public void should_return_the_list_of_students_who_study_all_day(string[] morningClass, string[] eveningClass, string[] expected)
+        {
+            // Dado / Setup
+            var exercises = new Prova();
+
+            // Quando / Ação
+            var returnedValue = exercises.Exercise8(morningClass, eveningClass);
+
+            // Deve / Asserções
+            Assert.Equal(expected, returnedValue);
+        }
+
     }
 }
